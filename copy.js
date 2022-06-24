ce3505d418b692fbbfab70ec4769e4c031d6097a
ZiChenStudio Officia <105914059+ZiChenStudio@users.noreply.github.com>
2022-06-24 20:04:59 +0800

    Create copy.js

---
+++ copy.js
@@ -0,0 +1,17 @@
+//网站禁止选择文字
+    document.body.onselectstart = function() {
+    self.event.returnValue=false
+};
+document.onkeydown = function(){
+    //禁止 F12
+    if (window.event && window.event.keyCode == 123) {
+    event.keyCode = 0;
+    event.returnValue = false;
+    }
+    }
+document.onkeydown = function(){
+    //禁止ctrl+u
+    if (event.ctrlKey && window.event.keyCode==85){
+    return false;
+    }
+    }
